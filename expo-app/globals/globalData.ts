59f57b993106981262cd6ca93cedbf2798851fe5
tattoomozi <tattoomozi@icloud.com>
2023-06-24 20:03:15 +0200

    Removing baseUrl

--- expo-app/globals/globalData.ts
+++ expo-app/globals/globalData.ts
@@ -1,13 +1,5 @@
 import Constants from 'expo-constants';
 
-/* const manifest = Constants;
-const apiBaseUrl = 'http://192.168.1.169:3000/api';
-export const apiBaseUrl =
-  typeof manifest.packagerOpts === `object` && manifest.packagerOpts.dev
-    ? `http://${manifest.debuggerHost.split(`:`).shift()}:3000/api`
-    : 'https://api.example.com';
-console.log(apiBaseUrl); */
-
 export const colors = {
   patternColorA: 'blue',
   patternColorB: 'orange',
